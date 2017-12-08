2c31638867cb84db632feddbe00a9ad9c71b9187
Cooper Bell <john.cooper.bell@gmail.com>
2017-12-08 11:35:14 -0600

    Finish Day 8

---
+++ day8/part1.hs
@@ -0,0 +1,79 @@
+module Main (main) where
+
+import Control.Applicative
+import qualified Data.Map.Strict as M
+import Data.List
+import Data.Maybe
+import Data.Ord
+import Text.ParserCombinators.ReadP
+
+type Input = (String, String, Int, String, String, Int)
+type Registers = M.Map String Int
+
+data Op = Op
+  { targetReg :: String
+  , condReg :: String
+  , op :: Int -> Int -> Int
+  }
+
+main :: IO ()
+main = maxVal . runOps . parseInput <$> readFile "input" >>= print
+
+maxVal :: Registers -> Int
+maxVal = snd . maximumBy (comparing snd) . M.toList
+
+runOps :: [Op] -> Registers
+runOps = foldl' runOp M.empty
+  where
+    runOp m (Op t c op) =
+      let tr = fromMaybe 0 (M.lookup t m)
+          cr = fromMaybe 0 (M.lookup c m)
+      in M.insert t (op tr cr) (M.insert c cr m)
+
+parseInput :: String -> [Op]
+parseInput = map (fst . head . readP_to_S (toOp parseLine)) . lines
+
+toOp :: ReadP Input -> ReadP Op
+toOp ip = ip >>= \(tr, top, tv, cr, cop, cv) ->
+  return $ Op tr cr (getOp top tv cop cv)
+  where
+    getOp top tv cop cv = \newTv newCv ->
+      if (opMap M.! cop) newCv cv
+      then getTOp top tv newTv
+      else newTv
+
+    getTOp o v = if o == "inc" then (+ v) else (subtract v)
+
+    opMap = M.fromList $
+      [ ("<",  (<))
+      , ("<=", (<=))
+      , (">",  (>))
+      , (">=", (>=))
+      , ("==", (==))
+      , ("!=", (/=))
+      ]
+
+parseLine :: ReadP Input
+parseLine = do
+  r <- asciiStr
+  skipSpaces
+  o <- string "inc" <|> string "dec"
+  skipSpaces
+  amt <- number
+  _ <- string " if "
+  cr <- asciiStr
+  skipSpaces
+  cop <- operation
+  skipSpaces
+  cov <- number
+  eof
+  return (r, o, amt, cr, cop, cov)
+  where
+    asciiStr = munch1 (`elem` ['a'..'z'])
+
+    number = do
+      sign <- option "" (string "-")
+      n <- munch1 (`elem` ['0'..'9'])
+      return $ read (sign ++ n)
+
+    operation = munch1 (`elem` "<>=!")
